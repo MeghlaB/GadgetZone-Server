ef418bbf401156f5e6e5b24fb4396414c36b1d94
MeghlaB <meghlab964@gmail.com>
2025-05-19 19:54:28 +0600

    added

--- index.js
+++ index.js
@@ -1,2 +1,69 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://e-commerce:xdO1nyHPbK5hFF4b@cluster0.t97zyjb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const usersCollection = client.db("Ecommerce").collection("users");
+
+    // users post collection api
+    app.post("/users", async (req, res) => {
+      const userData = req.body;
+
+      const query = { email: userData.email };
+      const exitingUser = await usersCollection.findOne(query);
+      if (exitingUser) {
+        return res.send({ message: "user already exits", instertedId: null });
+      }
+      const result = await usersCollection.insertOne(userData);
+      console.log(result);
+      res.send(result);
+    });
+
+    // user get collection api
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+  console.log(result);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("E-commerce api loading..............!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
