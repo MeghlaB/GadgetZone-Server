bf29e965df45fdc90e18170484f58eee22e21b0a
MeghlaB <meghlab964@gmail.com>
2025-08-20 01:39:38 +0600

    added

--- index.js
+++ index.js
@@ -265,8 +265,8 @@ async function run() {
         total_amount:product?.price,
         currency:order?.currency ,
         tran_id: tran_id, // use unique tran_id for each api call
-        success_url:`http://localhost:5000/payment/success/${tran_id}`,
-        fail_url: `http://localhost:5000/payment/fail/${tran_id}`,
+        success_url:`https://gadgetzone-server.onrender.com/payment/success/${tran_id}`,
+        fail_url: `https://gadgetzone-server.onrender.com/payment/fail/${tran_id}`,
         cancel_url: 'http://localhost:3030/cancel',
         ipn_url: 'http://localhost:3030/ipn',
         shipping_method: 'Courier',
